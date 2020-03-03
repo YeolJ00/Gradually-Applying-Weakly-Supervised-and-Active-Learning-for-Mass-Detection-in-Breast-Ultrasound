ad83c49490c4f95ed05b0430f318da33fa5c0ac2
YeolJ00 <karegun@gmail.com>
2020-03-03 15:01:31 +0900

    init weight for rcnn top

--- lib/model/faster_rcnn/faster_rcnn.py
+++ lib/model/faster_rcnn/faster_rcnn.py
@@ -191,6 +191,7 @@ class _fasterRCNN(nn.Module):
         # torch.nn.init.xavier_normal_(m.weight)
         m.weight.data.normal_(mean, stddev)
         m.bias.data.zero_()
+        
     def weight_Sequential(m):
       if type(m) == nn.Linear:
         m.weight.data.normal_(0, 0.001)
@@ -200,7 +201,7 @@ class _fasterRCNN(nn.Module):
     normal_init(self.RCNN_rpn.RPN_bbox_pred, 0, 0.01, cfg.TRAIN.TRUNCATED)
     normal_init(self.RCNN_cls_score, 0, 0.01, cfg.TRAIN.TRUNCATED)
     normal_init(self.RCNN_bbox_pred, 0, 0.001, cfg.TRAIN.TRUNCATED)
-    self.RCNN_top.apply()
+    self.RCNN_top.apply(weight_Sequential)
 
   def create_architecture(self):
     self._init_modules()
