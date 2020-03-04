e07469d3377b6ddaad3543ab284eea1937dc4be2
YeolJ00 <karegun@gmail.com>
2020-03-04 21:15:21 +0900

    use the original vgg16

--- lib/model/faster_rcnn/vgg16.py
+++ lib/model/faster_rcnn/vgg16.py
@@ -53,15 +53,17 @@ class vgg16(_fasterRCNN):
     # else:
     #   self.RCNN_bbox_pred = nn.Linear(4096, 4 * self.n_classes)    
 
-    self.RCNN_top = nn.Sequential(nn.Linear(25088,512), nn.ReLU(), nn.Dropout(p=0.5), nn.Linear(512,512),nn.Dropout(p=0.5), nn.ReLU())
+    # self.RCNN_top = nn.Sequential(nn.Linear(25088,512), nn.ReLU(), nn.Dropout(p=0.5), nn.Linear(512,512),nn.Dropout(p=0.5), nn.ReLU())
+    self.RCNN_top = vgg.classifier
 
+    #XXX modified 4096 -> 512
     # not using the last maxpool layer
-    self.RCNN_cls_score = nn.Linear(512, self.n_classes)
+    self.RCNN_cls_score = nn.Linear(4096, self.n_classes)
 
     if self.class_agnostic:
-      self.RCNN_bbox_pred = nn.Linear(512, 4)
+      self.RCNN_bbox_pred = nn.Linear(4096, 4)
     else:
-      self.RCNN_bbox_pred = nn.Linear(512, 4 * self.n_classes)
+      self.RCNN_bbox_pred = nn.Linear(4096, 4 * self.n_classes)
 
   def _head_to_tail(self, pool5):
 
