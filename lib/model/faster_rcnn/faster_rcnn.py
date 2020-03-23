a669259ec3f310de52158df314ae5fffeff3aeaf
YeolJ00 <karegun@gmail.com>
2020-03-23 17:57:53 +0900

    pyc changes

--- lib/model/faster_rcnn/faster_rcnn.py
+++ lib/model/faster_rcnn/faster_rcnn.py
@@ -194,10 +194,10 @@ class _fasterRCNN(nn.Module):
     
     def weight_Sequential(m, mean, stddev, trauncated=False):
       classname = m.__class__.__name__
-      if classname.find('Conv') != -1:
-        torch.nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
-        # m.weight.data.normal_(0.0, 0.02)
-        # m.bias.data.fill_(0)
+      if classname.find('Linear') != -1:
+        # torch.nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+        m.weight.data.normal_(0.0, 0.01)
+        m.bias.data.fill_(0)
       elif classname.find('BatchNorm') != -1:
         # m.weight.data.normal_(1.0, 0.02)
         # m.bias.data.fill_(0)
