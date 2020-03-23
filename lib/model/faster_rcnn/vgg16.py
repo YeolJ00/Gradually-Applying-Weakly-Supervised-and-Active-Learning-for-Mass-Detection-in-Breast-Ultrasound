9f61fa41db4832577d02d113054839d87e2bd461
YeolJ00 <karegun@gmail.com>
2020-03-23 18:41:07 +0900

    pyc changes

--- lib/model/faster_rcnn/vgg16.py
+++ lib/model/faster_rcnn/vgg16.py
@@ -53,15 +53,19 @@ class vgg16(_fasterRCNN):
     # else:
     #   self.RCNN_bbox_pred = nn.Linear(4096, 4 * self.n_classes)      
 
-    self.RCNN_top = nn.Sequential(nn.Linear(25088, 512),nn.ReLU(inplace=True),nn.Dropout(p=0.5), nn.Linear(512,512), nn.ReLU(inplace=True), nn.Dropout(p=0.5))
+    self.RCNN_top = nn.Sequential(nn.Linear(25088, 4096),nn.ReLU(inplace=True),nn.Dropout(p=0.5), nn.Linear(4096,2048), nn.ReLU(inplace=True), nn.Dropout(p=0.5))
+    # self.RCNN_top = nn.Sequential(nn.Linear(25088, 512),nn.ReLU(inplace=True),nn.Dropout(p=0.5), nn.Linear(512,512), nn.ReLU(inplace=True), nn.Dropout(p=0.5))
 
     # not using the last maxpool layer
-    self.RCNN_cls_score = nn.Linear(512, self.n_classes)
+    self.RCNN_cls_score = nn.Linear(2048, self.n_classes)
+    # self.RCNN_cls_score = nn.Linear(512, self.n_classes)
 
     if self.class_agnostic:
-      self.RCNN_bbox_pred = nn.Linear(512, 4)
+      self.RCNN_bbox_pred = nn.Linear(2048, 4)
+      # self.RCNN_bbox_pred = nn.Linear(512, 4)
     else:
-      self.RCNN_bbox_pred = nn.Linear(512, 4 * self.n_classes)      
+      self.RCNN_bbox_pred = nn.Linear(2048, 4 * self.n_classes)
+      # self.RCNN_bbox_pred = nn.Linear(512, 4 * self.n_classes)
 
   def _head_to_tail(self, pool5):
     
